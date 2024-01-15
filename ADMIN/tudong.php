8b83ff6180b494a2d4840b27d89fc127a2e3cb73
Thuan <vuonghuythuan1@gmail.com>
2024-01-15 23:02:12 +0700

    thuan

--- ADMIN/tudong.php
+++ ADMIN/tudong.php
@@ -1,24 +1,27 @@
 <?php
-require 'vendor/autoload.php';  // Đường dẫn đến autoload.php của PHPMailer
+
+require 'vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 function sendWeeklyEmail() {
     $mail = new PHPMailer(true);
-    
+
     try {
+        // Enable verbose debugging
+        // $mail->SMTPDebug = 2;
+
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'vuonghuythuan1@gmail.com'; // Thay đổi địa chỉ email người gửi
-        $mail->Password = 'lfpg krbl kyae wnum'; // Thay đổi mật khẩu email người gửi
+        $mail->Username = 'vuonghuythuan1@gmail.com';
+        $mail->Password = 'your_gmail_password';
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
-        $mail->setFrom('vuonghuythuan1@gmail.com', 'Your Name'); // Thay đổi địa chỉ email người gửi
+        $mail->setFrom('vuonghuythuan1@gmail.com', 'Your Name');
 
-        // Thêm địa chỉ email người nhận
         $recipients = ['nguyenbatung@gmail.com'];
         foreach ($recipients as $recipient) {
             $mail->addAddress($recipient);
@@ -33,7 +36,6 @@ function sendWeeklyEmail() {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
-
 
 sendWeeklyEmail();
 ?>
