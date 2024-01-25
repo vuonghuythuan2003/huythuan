8e6f1f2d9cfeac1906ce7ee4c7ab47096a6c1a6f
Nguyen Ba Tung <batungnguyen78@gmail.com>
2024-01-25 13:08:54 +0700

    tung

--- ADMIN/login.php
+++ ADMIN/login.php
@@ -1,108 +1,70 @@
 <?php
-session_start();
+  session_start();
+  if(isset($_POST['nutdangnhap']) == true) {
+      $username=$_POST['username'];
+      $password=$_POST['password'];
+      $conn = new PDO("mysql:host=localhost; dbname=nckh; charset=utf8", "root"); 
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      
+      $sql = "SELECT * FROM t_user WHERE username=? AND password=? AND active=1";
+      $st=$conn->prepare($sql);
+      $st->execute([$username,$password]);
 
-// Hàm kiểm tra đăng nhập
-function isUserLoggedIn()
-{
-    return isset($_SESSION['user_id']) && $_SESSION['user_id'] > 0 && $_SESSION['session_id'] == session_id();
-}
+      if ($st->rowCount() == 1) {
+          $user = $st->fetch();
+          $idgroup = $user['idgroup'];
+          
+          if ($idgroup == 0) {
+              $_SESSION['login_id'] = $user['id'];
+              $_SESSION['login_user'] = $user['username'];
+              $_SESSION['login_fullname'] = $user['fullname'];
+              header('location: ../views/trangchu.php');
+              exit();
+          } elseif ($idgroup == 1) {
+              $_SESSION['admin_id'] = $user['id'];
+              $_SESSION['admin_user'] = $user['username'];
+              $_SESSION['admin_fullname'] = $user['fullname'];
+              header('location: ../views/trangchuadmin.php');
+              exit();
+          } else {
+              echo "Unknown user role";
+          }
+      } else {
+          echo "Mật khẩu hoặc tài khoản đăng nhập sai";
+      }
+  }
+  ?>
 
-// Hàm xác thực tài khoản
-function authenticateUser($conn, $username, $password)
-{
-    $sql = "SELECT * FROM t_user WHERE username = ? AND password = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$username, $password]);
 
-    if ($stmt->rowCount() == 1) {
-        $user = $stmt->fetch();
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
-        $_SESSION['fullname'] = $user['fullname'];
-        $_SESSION['session_id'] = session_id(); // Lưu session_id
-        return true;
-    } else {
-        return false;
-    }
-}
 
-// Thông tin kết nối đến CSDL
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "nckh";
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Trang Đăng nhập</title>
+      <link rel="stylesheet" href="../CSS/login.css">
 
-try {
-    // Kết nối đến CSDL
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  </head>
+  <body>
+        <form method="post">
+            <div class="vid-container">
+                <video id="Video1" class="bgvid back" autoplay="true" muted="muted" preload="auto" loop>
+                    <source src="../IMAGE/vid.mp4" type="video/mp4">
+                </video>
+                <div class="inner-container">
+                    <div class="box">
+                        <h1 style="color:#fff">Login</h1>
+                        <input value="<?php if(isset($username)==true )echo $username;?>" for="username"  type="text" class="form-control" id="username" name="username" placeholder="Nhập tên đăng nhập của bạn">
+                        <input value="<?php if(isset($password)==true )echo $password;?>" for="password" type="password" class="form-control" id="password" name="password" autocomplete=”new-password” placeholder="Nhập mật khẩu của bạn">
+                        <button type="submit" name="nutdangnhap" class="btn btn-primary">Login</button>
+                        <p>Not a member? <span class="signup"><a href ="signup.php">Sign Up</a></span></p>
+                        <p><span class="signup"><a href ="quenmk.php">Quên mật khẩu</a></span></p>
 
-    // Kiểm tra xem tài khoản có đang hoạt động hay không
-    if (isUserLoggedIn()) {
-        // Tài khoản đang hoạt động, chuyển hướng tới trang header.php
-        header('Location: ../views/testform.php');
-        exit();
-    } else {
-        // Tài khoản không đang hoạt động
-        // Yêu cầu người dùng đăng nhập hoặc thực hiện xác thực tài khoản
-        if (isset($_POST['login_button'])) {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-
-            if (authenticateUser($conn, $username, $password)) {
-                // Đăng nhập thành công, chuyển hướng trực tiếp đến trang header.php
-                header('Location: ../views/header.php');
-                exit();
-            } else {
-                // Xác thực không thành công, thông báo lỗi hoặc yêu cầu đăng nhập lại
-                echo "Mật khẩu hoặc tên đăng nhập không đúng.";
-            }
-        } else {
-            // Hiển thị biểu mẫu đăng nhập
-?>
-            <!DOCTYPE html>
-            <html lang="en">
-
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Trang Đăng nhập</title>
-                <link rel="stylesheet" href="../CSS/login.css">
-            </head>
-
-            <body>
-                <form method="post" action="">
-                    <div class="vid-container">
-                        <video id="Video1" class="bgvid back" autoplay="true" muted="muted" preload="auto" loop>
-                            <source src="../IMAGE/vid.mp4" type="video/mp4">
-                        </video>
-                        <div class="inner-container">
-                            <div class="box">
-                                <h1 style="color:#fff">Login</h1>
-                                <!-- <label for="username">Tên đăng nhập:</label> -->
-                                <input type="text" id="username" name="username" placeholder="Nhập tên đăng nhập của bạn" required><br>
-
-                                <!-- <label for="password">Mật khẩu:</label> -->
-                                <input type="password" id="password" name="password" placeholder="Nhập mật khẩu của bạn" required><br>
-
-                                <button type="submit" name="login_button" class="btn btn-primary">Login</button>
-                                <p>Not a member? <span class="signup"><a href="signup.php">Sign Up</a></span></p>
-                                <p><span class="signup"><a href="quenmk.php">Quên mật khẩu</a></span></p>
-                            </div>
-                        </div>
                     </div>
-                </form>
-            </body>
+                </div>
+            </div>
+        </form>
 
-            </html>
-
-<?php
-        }
-    }
-} catch (PDOException $e) {
-    echo "Lỗi kết nối CSDL: " . $e->getMessage();
-}
-
-// Đóng kết nối
-$conn = null;
-?>
+  </body>
+  </html>
--- views/baiviet1.php
+++ views/baiviet1.php
@@ -5,105 +5,82 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chữa lành tổn thương tâm lý</title>
-    <link rel="stylesheet" href="../CSS/index.css">
-    <link rel="stylesheet" href="../CSS/nav.css">
-    <link rel="stylesheet" href="../CSS/bootstrap.min.css">
-    <script src="../JS/jquery-3.4.1.js"></script>
-    <script src="../JS/popper.min.js"></script>
-    <script src="../JS/bootstrap.bundle.min.js"></script>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 20px;
+        }
+
+        h1, h2, h3, p {
+            margin-bottom: 15px;
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+
+        form {
+            text-align: left;
+        }
+    </style>
 </head>
 
 <body>
-    <ul class="hmenu">
-        <li><a href="#">Trang chủ</a></li>
-        <li><a href="#">Tổng quan</a>
-            <ul class="sub-menu">
-                <li><a href="#">Nguyên nhân</a></li>
-                <li><a href="#">Đặc điểm</a></li>
-                <li><a href="#">Triệu chứng</a></li>
-                <li><a href="#">Đối tượng</a></li>
-            </ul>
-        </li>
-        <li><a href="lienhe.php">Liên hệ</a></li>
+    <?php
+    include "menu.html"
+    ?>
+    <br><br>
+    <h1>4 Phương pháp chữa lành tổn thương tâm lý</h1>
+    <p>Bạn có đang cảm thấy đau buồn, lo âu, trầm cảm? Hãy thử 4 phương pháp chữa lành tổn thương tâm lý sau đây nhé.</p>
+    <h2>Tổn thương tâm lý là gì?</h2>
+    <p>
+        Tổn thương tâm lý là một loại tổn thương về tinh thần và cảm xúc. Người bị tổn thương tâm lý thường gặp khó khăn trong cuộc sống khi khó duy trì được những mối quan hệ, không kiểm soát được cảm xúc của bản thân, khó khăn khi tiếp nhận nhiều sự việc hay con người, đặc biệt có thể dẫn đến những nguy cơ về sức khỏe thể chất.
+    </p>
 
-        <li><a href="#">Giải pháp</a>
-            <ul class="sub-menu">
-                <li><a href="baiviet.php">Bài viết</a></li>
-                <li><a href="video.php">Video</a></li>
-                <li><a href="game.php">Game</a></li>
-            </ul>
-        </li>
-        <li><a href="#">Bài Test</a></li>
+    <h2>Những dấu hiệu nào cho thấy bạn đang bị tổn thương tâm lý</h2>
+    <p>
+        Những tổn thương tâm lý này có thể đến từ những sự kiện đặc biệt nào đó như thất bại trong kinh doanh, thua lỗ, li hôn, bị bạo lực, thất tình… hoặc tích tụ trong thời gian dài. Những tổn thương này có thể là trầm cảm, căng thẳng, lo âu, sợ xã hội…khiến người bệnh tự ti, tiêu cực và buồn phiền.
+    </p>
 
-        <li style="float: right; margin-right: 0px;">
-            <button id="submit" style="padding: 10px 20px; color: white; background-color: orange; border: solid white;">
-                <a href="#" style="text-decoration: none; color: #ffffff;">Đăng xuất</a>
-            </button>
-        </li>
+    <h3>Dưới đây là những dấu hiệu cho thấy bạn có thể đang gặp tổn thương tâm lý:</h3>
+    <ul>
+        <li><strong>Tính cách và tâm trạng biến đổi bất thường:</strong> Sự biến đổi này có thể rõ ràng hoặc không, nhưng lại đột ngột, ảnh hưởng đến hành vi và tính cách thường ngày.</li>
+        <li><strong>Hay tức giận, khó kiểm soát cảm xúc:</strong> Do tổn thương tâm lý, gây ra nhiều lo âu căng thẳng, dẫn đến việc kiểm soát gặp khó khăn, hành vi trở nên bốc đồng. Việc này có thể ảnh hưởng lớn đến các mối quan hệ xung quanh bạn.</li>
+        <li><strong>Hay lo lắng, mệt mỏi, thiếu năng lượng, tiêu cực và tự thu mình lại:</strong> Đây là những dấu hiệu thường thấy ở người bị tổn thương tâm lý. Nếu bạn bị tổn thương về tâm lý, có thể bạn sẽ không gặp phải tất cả những dấu hiệu này, nhưng có lẽ sẽ thấy ít nhất.</li>
     </ul>
 
-    <div style="display: flex; justify-content: space-between; margin: 20px;">
-        <div style="flex: 1;">
-            <h1 style="text-align: center;">4 Phương pháp chữa lành tổn thương tâm lý</h1><br>
-            <p>
-                Bạn có đang cảm thấy đau buồn, lo âu, trầm cảm? Hãy thử 4 phương pháp chữa lành tổn thương tâm lý sau đây nhé.
-            </p>
-        <img src='../IMAGE/4pchualanh.png' alt='Mô tả ảnh' style='width: 100%; max-width: 400px; height: auto;'>
-        <p>
-        <h2>Tổn thương tâm lý là gì?</h2><br>
-Tổn thương tâm lý là một loại tổn thương về tinh thần và cảm xúc. Người bị tổn thương tâm lý thường gặp khó khăn trong cuộc sống khi khó duy trì được những mối quan hệ, không kiểm soát được cảm xúc của bản thân, khó khăn khi tiếp nhận nhiều sự việc hay con người, đặc biệt có thể dẫn đến những nguy cơ về sức khỏe thể chất.<br>
+    <h2>Phương pháp chữa lành tổn thương tâm lý</h2>
+    <p>
+        <strong>Tự nhận thức và muốn thay đổi</strong><br>
+        Mỗi người sẽ có cách để đối mặt với những tổn thương tâm lý khác nhau. Khi gặp tổn thương về tâm lý, rất nhiều người lựa chọn trốn tránh, mặc kệ. Nhưng nếu bạn không thể tự nhận thức được rằng mình cần chữa lành tổn thương tâm lý thì tình trạng này rất khó để cải thiện.<br>
 
-<h2>Những dấu hiệu nào cho thấy bạn đang bị tổn thương tâm lý</h2><br>
-Những tổn thương tâm lý này có thể đến từ những sự kiện đặc biệt nào đó như thất bại trong kinh doanh, thua lỗ, li hôn, bị bạo lực, thất tình… hoặc tích tụ trong thời gian dài. Những tổn thương này có thể là trầm cảm, căng thẳng, lo âu, sợ xã hội…khiến người bệnh tự ti, tiêu cực và buồn phiền.<br>
+        Việc đầu tiên cần làm là nhận ra vết thương tâm lý, tốt hơn nữa là hiểu được nguyên nhân dẫn đến những tổn thương này. Đây cũng là cách để bạn tự yêu thương bản thân mình nhiều hơn. Từ đó không ngại để thử các phương pháp chữa lành tổn thương tâm lý bên dưới.
+    </p>
 
-<h2>Dưới đây là những dấu hiệu cho thấy bạn có thể đang gặp tổn thương tâm lý:</h2><br>
+    <p>
+        <strong>Làm những điều gì đó thú vị mỗi ngày</strong><br>
+        Hãy “rà soát” lại những thứ có thể khiến bạn thấy hứng thú, vui vẻ. Cố gắng thực hiện các hoạt động đó mỗi ngày hoặc mỗi tuần, như một cách nuôi dưỡng sự tích cực. Bạn hoàn toàn cũng có thể thử những điều mới lạ, một số hoạt động thú vị cần sự sáng tạo. Đây cũng là cách giúp cải thiện sức khỏe tinh thần được các chuyên gia tâm lý ủng hộ.<br>
 
-<h3>Tính cách và tâm trạng biến đổi bất thường:</h3><br>
-Sự biến đổi này có thể rõ ràng hoặc không, nhưng lại đột ngột, ảnh hưởng đến hành vi và tính cách thường ngày<br>
+        Bạn có thể tìm kiếm và làm quen với môi trường hoàn toàn mới, tiếp xúc với những người “xa lạ” nếu bạn cảm thấy quanh mình không có ai đủ tin tưởng. Những người xa lạ không biết gì về mình có lẽ sẽ mang lại những suy nghĩ mới, cảm xúc mới thay đổi tinh thần của bạn.
+    </p>
 
-<h3>Hay tức giận, khó kiểm soát cảm xúc:</h3><br>
-Do tổn thương tâm lý, gây ra nhiều lo âu căng thẳng, dẫn đến việc kiểm soát gặp khó khăn, hành vi trở nên bốc đồng. Việc này có thể ảnh hưởng lớn đến các mối quan hệ xung quanh bạn.<br>
-<h3>Hay lo lắng, mệt mỏi, thiếu năng lượng, tiêu cực và tự thu mình lại:</h3><br>
-Đây là những dấu hiệu thường thấy ở người bị tổn thương tâm lý. Nếu bạn bị tổn thương về tâm lý, có thể bạn sẽ không gặp phải tất cả những dấu hiệu này, nhưng có lẽ sẽ thấy ít nhất<br>
+    <p>
+        <strong>Cải thiện sức khỏe thể chất</strong><br>
+        Có một cơ thể khỏe mạnh và tràn đầy năng lượng cũng giúp bạn hạn chế được sự mệt mỏi về tinh thần. Bạn có thể lựa chọn các hoạt động phù hợp như đi bộ, yoga, thiền, ăn uống lành mạnh và ngủ nghỉ đầy đủ.<br>
 
-<h2>Phương pháp chữa lành tổn thương tâm lý</h2><br>
-Tự nhận thức và muốn thay đổi<br>
-Mỗi người sẽ có cách để đối mặt với những tổn thương tâm lý khác nhau. Khi gặp tổn thương về tâm lý, rất nhiều người lựa chọn trốn tránh, mặc kệ. Nhưng nếu bạn không thể tự nhận thức được rằng mình cần chữa lành tổn thương tâm lý thì tình trạng này rất khó để cải thiện.<br>
+        Nếu bạn gặp phải căng thẳng, cũng có thể thử các phương pháp massage, xông hơi để thư giãn đầu óc.
+    </p>
 
-Việc đầu tiên cần làm là nhận ra vết thương tâm lý, tốt hơn nữa là hiểu được nguyên nhân dẫn đến những tổn thương này. Đây cũng là cách để bạn tự yêu thương bản thân mình nhiều hơn. Từ đó không ngại để thử các phương pháp chữa lành tổn thương tâm lý bên dưới<br>
+    <h2>Phương pháp chữa lành tổn thương tâm lý</h2>
 
-Làm những điều gì đó thú vị mỗi ngày<br>
-Hãy “rà soát” lại những thứ có thể khiến bạn thấy hứng thú, vui vẻ. Cố gắng thực hiện các hoạt động đó mỗi ngày hoặc mỗi tuần, như một cách nuôi dưỡng sự tích cực. Bạn hoàn toàn cũng có thể thử những điều mới lạ, một số hoạt động thú vị cần sự sáng tạo. Đây cũng là cách giúp cải thiện sức khỏe tinh thần được các chuyên gia tâm lý ủng hộ.<br>
+    <p>
+        <strong>Gặp gỡ các nhà trị liệu tâm lý</strong><br>
+        Nếu bạn cảm thấy mình không thể “tự chữa lành” hay đang hoang mang không biết phải thực hiện phương pháp nào, đâu mới là lựa chọn phù hợp thì hãy để các nhà trị liệu tâm lý giúp đỡ bạn.<br>
 
-Bạn có thể tìm kiếm và làm quen với môi trường hoàn toàn mới, tiếp xúc với những người “xa lạ” nếu bạn cảm thấy quanh mình không có ai đủ tin tưởng. Những người xa lạ không biết gì về mình có lẽ sẽ mang lại những suy nghĩ mới, cảm xúc mới thay đổi tinh thần của bạn.<br>
-
-Cải thiện sức khỏe thể chất<br>
-Có một cơ thể khỏe mạnh và tràn đầy năng lượng cũng giúp bạn hạn chế được sự mệt mỏi về tinh thần. Bạn có thể lựa chọn các hoạt động phù hợp như đi bộ, yoga, thiền, ăn uống lành mạnh và ngủ nghỉ đầy đủ.<br>
-
-Nếu bạn gặp phải căng thẳng, cũng có thể thử các phương pháp massage, xông hơi để thư giãn đầu óc.<br>
-Gặp gỡ các nhà trị liệu tâm lý<br>
-Nếu bạn cảm thấy mình không thể “tự chữa lành” hay đang hoang mang không biết phải thực hiện phương pháp nào, đâu mới là lựa chọn phù hợp thì hãy để các nhà trị liệu tâm lý giúp đỡ bạn.<br>
-
-Nhà trị liệu sẽ đánh giá tình trạng tâm lý, mức độ tổn thương của bạn để thiết kế liệu trình trị liệu phù hợp. Đồng thời, họ cũng là người đồng hành giúp bạn thay đổi nhận thức, từ từ giải quyết những rào cản tâm lý, cho những tổn thương dần dần phục hồi.<br>";
-
-</p>
-    </div>
-    <div style="flex: 1; padding-left: 20px;">
-            <!-- Form điền thông tin -->
-            <form action="#" method="post">
-                <label for="ten">Tên:</label>
-                <input type="text" id="ten" name="ten"><br>
-
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email"><br>
-
-                <input type="submit" value="Gửi">
-            </form>
-        </div>
-    </div>
-</body>
-
-</html>
+        Nhà trị liệu sẽ đánh giá tình trạng tâm lý, mức độ tổn thương của bạn để thiết kế liệu trình trị liệu phù hợp. Đồng thời, họ cũng là người đồng hành giúp bạn thay đổi nhận thức, từ từ giải quyết những rào cản tâm lý, cho những tổn thương dần dần phục hồi.
+    </p>
 </body>
 
 </html>
